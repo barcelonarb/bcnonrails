625f8ca31273141b7ec3b19b4537f6eeeefebaf9
jrd <showlnx@gmail.com>
2009-11-14 01:30:32 +0100

    Eventos: Image trigger

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
-	$("#datetime").datetimepicker();
+	$("#datetime").datetimepicker({showOn: 'button', buttonImage: '/images/calendar.gif', buttonImageOnly: true });
 });
