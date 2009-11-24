49f32e6268b9c35e92071d2211593eb9a6075b66
jrd <showlnx@gmail.com>
2009-11-24 23:39:03 +0100

    Sitemap

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -4,8 +4,8 @@ class MiscController < ApplicationController
     @event = Event.next.first
     @users = User.random(36)
     @companies = Company.random(10)
-    @jobs = Job.find(:all, :order => 'created_at', :limit => 10)
-    @events = Event.next.all(10)
+    @jobs = Job.find(:all, :order => 'created_at desc', :limit => 10)
+    @events = Event.next.all(:limit => 10)
   end
 
 end
