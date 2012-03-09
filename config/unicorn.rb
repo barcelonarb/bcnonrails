404f45b69e23ab83b34172b39bd76c6f8c0fc101
Victor Castell <victorcoder@gmail.com>
2012-03-09 12:42:37 +0100

    Typo in unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -29,7 +29,7 @@ working_directory "#{deploy_path}/current" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/barcelonaonrails_#{environment}.sock", :backlog => 64
+listen "/tmp/barcelonaonrails.com_#{environment}.sock", :backlog => 64
 #listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
