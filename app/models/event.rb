58284072eb3e987f5427a02587146d3bcfdd0888
Victor Castell <victorcoder@gmail.com>
2013-04-07 23:57:23 +0200

    Using geocoder instead of ym4r_gm

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,15 @@
-require "geocodable"
+#require "geocodable"
 class Event < ActiveRecord::Base
-  include Geocodable
+  #include Geocodable
+  geocoded_by :location, :latitude  => :lat, :longitude => :lng
 
   validates_presence_of :date, :title, :location
 
   scope :next, :conditions => ['date > ?', DateTime.now - 2.hours], :order => 'date asc'
 
-  before_save :geocode_location
+  after_validation :geocode, :if => :location_changed?
   after_create :deliver_event_to_bcnonrails!
+
 
   def deliver_event_to_bcnonrails!
     Notifier.event_to_bcnonrails(self).deliver
