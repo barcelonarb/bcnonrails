c659b6ea11a95ecdd08ef1aa4d40cf41b86aafed
Victor Castell <victorcoder@gmail.com>
2013-04-07 23:24:38 +0200

    Just show 33 users in home

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,7 +2,7 @@ class MiscController < ApplicationController
   
   def home
     @event = Event.next.first
-    @users = User.random(36)
+    @users = User.random(33)
     @companies = Company.random(10)
     @jobs = Job.find(:all, :order => 'created_at desc', :limit => 10)
     @events = Event.next.all(:limit => 10)
