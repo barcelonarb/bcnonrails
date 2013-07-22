2e88f33de796a0fa3654fbc08f1780e1faf25369
Victor Castell <victorcoder@gmail.com>
2013-07-22 17:22:46 +0200

    Show last event in home page

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -5,7 +5,7 @@ class MiscController < ApplicationController
     @users = User.random(33)
     @companies = Company.random(10)
     @jobs = Job.find(:all, :order => 'created_at desc', :limit => 10)
-    @events = Event.next.all(:limit => 10)
+    @events = Event.all(:limit => 10)
   end
 
 end
